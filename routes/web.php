cc09571209322a98f51b751be26559de37f5c4d2
ONLINQ\bink <bink@onlinq.nl>
2024-07-16 12:23:30 +0200

    Database setup and task model

--- routes/web.php
+++ routes/web.php
@@ -2,65 +2,10 @@
 
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
+use App\Models\Task;
 
-class Task
-{
-    public function __construct(
-        public int $id,
-        public string $title,
-        public string $description,
-        public ?string $long_description,
-        public bool $completed,
-        public string $created_at,
-        public string $updated_at
-    ) {
-    }
-}
-
-$tasks = [
-    new Task(
-        1,
-        'Buy groceries',
-        'Task 1 description',
-        'Task 1 long description',
-        false,
-        '2023-03-01 12:00:00',
-        '2023-03-01 12:00:00'
-    ),
-    new Task(
-        2,
-        'Sell old stuff',
-        'Task 2 description',
-        null,
-        false,
-        '2023-03-02 12:00:00',
-        '2023-03-02 12:00:00'
-    ),
-    new Task(
-        3,
-        'Learn programming',
-        'Task 3 description',
-        'Task 3 long description',
-        true,
-        '2023-03-03 12:00:00',
-        '2023-03-03 12:00:00'
-    ),
-    new Task(
-        4,
-        'Take dogs for a walk',
-        'Task 4 description',
-        null,
-        false,
-        '2023-03-04 12:00:00',
-        '2023-03-04 12:00:00'
-    ),
-];
-
-
-
-
-Route::get('/', function() use($tasks) {
-    return view('index', ['tasks' => $tasks]);
+Route::get('/', function() {
+    return view('index', ['tasks' => Task::all()]);
 })->name('tasks.index');
 
 
@@ -68,11 +13,9 @@ Route::fallback(function() {
     return redirect()->route('tasks.index');
 });
 
-Route::get('/{id}', function($id) use($tasks) {
-    $task = collect($tasks)->firstwhere('id', $id);
-
+Route::get('/{id}', function($id) {
+    $task = Task::find($id);
     if(!isset($task) || !$task) {
-        // return redirect()->route('tasks.index');
         abort(Response::HTTP_NOT_FOUND);
     }
 
